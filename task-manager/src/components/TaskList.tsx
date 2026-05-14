e4f28ec23e33b719f0ee4af1a60b05a12b42b65b
Sharmaprateek52 <prateekkr12@gmail.com>
2026-05-15 00:37:17 +0530

    formatting fix

--- task-manager/src/components/TaskList.tsx
+++ task-manager/src/components/TaskList.tsx
@@ -4,15 +4,19 @@ import TaskItem from "./TaskItem";
 interface Props {
   tasks: Task[];
   onDeleteTask: (id: number) => void;
-    onCompleteTask: (id: number) => void;
-
+  onCompleteTask: (id: number) => void;
 }
 
 function TaskList({ tasks, onDeleteTask, onCompleteTask }: Props) {
   return (
     <div>
       {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} onDeleteTask={onDeleteTask} onCompleteTask={onCompleteTask} />
+        <TaskItem
+          key={task.id}
+          task={task}
+          onDeleteTask={onDeleteTask}
+          onCompleteTask={onCompleteTask}
+        />
       ))}
     </div>
   );
